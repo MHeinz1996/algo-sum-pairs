6e6933c845ecb0413af4060fc08401382cef1fce
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-25 16:50:56 -0500

    Completed algo

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,16 @@
-def sum_pairs():
+def sum_pairs(ls, target):
+    sums = []
+    for i in range(0,len(ls)):
+        for j in range(1, len(ls)):
+            if(i<j):
+                if(ls[i]+ ls[j] == target):
+                    sums.append([ls[i], ls[j]])
+
+    if(len(sums) == 0):
+        return("unable to find pairs")
+    else:
+        return sums
+
+print(sum_pairs([1, 2, 3, 4, 5], 9))
+print(sum_pairs([1,2,3,4,5], 7))
+print(sum_pairs([3, 1, 5, 8, 2], 27))
