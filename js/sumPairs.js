ac853acda94945362f888f7f86628d1fceb8c72a
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-27 09:10:15 -0500

    Installed deep-equal and was able to pass Spec tests

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -22,12 +22,12 @@ exports.sumPairs = function(arr, target) {
 // console.log(exports.sumPairs([1,2,3,4,5], 7));  // [[2,5], [3,4]]
 // console.log(exports.sumPairs([3, 1, 5, 8, 2], 27)); // 'unable to find pairs'
 
-let test = exports.sumPairs([1,2,3,4,5], 9);
-console.log('Function answer:', test, '->', typeof test);
-console.log('Expected answer:', [[4,5]], '->', typeof ([[4,5]]));
+// let test = exports.sumPairs([1,2,3,4,5], 9);
+// console.log('Function answer:', test, '->', typeof test);
+// console.log('Expected answer:', [[4,5]], '->', typeof ([[4,5]]));
 
-if(test === [[4,5]]) {
-    console.log('True');
-} else {
-    console.log('False');
-}
+// if(test === [[4,5]]) {
+//     console.log('True');
+// } else {
+//     console.log('False');
+// }
