9382833d9e348e9cd7a8bab499759fc67d7c6852
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-27 00:43:45 -0500

    Algorithm should work, but doesn't for some reason

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,33 @@
-exports.sumPairs = function() {
+exports.sumPairs = function(arr, target) {
+    let pairs = [];
 
+    for(let i=0; i<arr.length; i++) {
+        for(let j=1; j<arr.length; j++) {
+            if(i < j) {
+                if((arr[i] + arr[j] === target)) {
+                    pairs.push([arr[i],arr[j]])
+                }
+            }
+        }
+    }
+    
+    if(pairs.length != 0){
+        return pairs
+    } else {
+        return ('unable to find pairs')
+    }
 };
+
+// console.log(exports.sumPairs([1,2,3,4,5], 9));  // [[4,5]]
+// console.log(exports.sumPairs([1,2,3,4,5], 7));  // [[2,5], [3,4]]
+// console.log(exports.sumPairs([3, 1, 5, 8, 2], 27)); // 'unable to find pairs'
+
+let test = exports.sumPairs([1,2,3,4,5], 9);
+console.log('Function answer:', test, '->', typeof test);
+console.log('Expected answer:', [[4,5]], '->', typeof ([[4,5]]));
+
+if(test === [[4,5]]) {
+    console.log('True');
+} else {
+    console.log('False');
+}
